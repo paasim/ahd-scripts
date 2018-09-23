827164676a208ea6b3d5e835b1e155408e20e48a
Markus Paasiniemi <markus.paasiniemi@cs.helsinki.fi>
2018-09-23 16:51:06 +0300

    fix hr.sh for non-integer heart rate measurements

--- hr.sh
+++ hr.sh
@@ -8,6 +8,6 @@ grep "HKQuantityTypeIdentifierHeartRate\|<low value\|<value xsi" $1 |
   # print <low value and <value after HKQuantity
   awk '/HeartRate/ {pr=3} pr>0 { pr-- ; print}' |
   # keep only the (numeric) values, tr used to remove unnecessary "
-  grep -o '[0-9+]\+' |
+  grep -o '[0-9+.]\+' |
   # transform to tabular format
   awk 'ORS=NR%2?FS:RS'
